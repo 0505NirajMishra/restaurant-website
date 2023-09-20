c9e16d2c32428bb610be98518040c834b5817ade
niraj mishra <nirajmishra8866@gmail.com>
2023-09-20 18:53:06 +0530

    changes on dropdown hover and mobile view and chnages on footer

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,43 @@
-
-  (function ($) {
-  
+(function ($) {
   "use strict";
 
-    // NAVBAR
-    $('.navbar-nav .nav-link').click(function(){
-        $(".navbar-collapse").collapse('hide');
-    });
-    
-  })(window.jQuery);
+  // NAVBAR
+  $(".navbar-nav .nav-link").click(function () {
+    $(".navbar-collapse").collapse("hide");
+  });
+})(window.jQuery);
 
-  
+function openTab(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+document.getElementById("defaultOpen").click();
+
+function readURL(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+    reader.onload = function (e) {
+      $("#imagePreview").css(
+        "background-image",
+        "url(" + e.target.result + ")"
+      );
+      $("#imagePreview").hide();
+      $("#imagePreview").fadeIn(650);
+    };
+    reader.readAsDataURL(input.files[0]);
+  }
+}
+$("#imageUpload").change(function () {
+  readURL(this);
+});
+
